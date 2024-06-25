c27dec9f4196c5afeee1566436761a91a5a2e28a
iuri grossi de lima <iuriscp@gmail.com>
2024-06-25 13:17:31 -0300

    partial feature Forgot password

--- auth.js
+++ auth.js
@@ -1,13 +1,32 @@
 //AUTENTICAÇÃO
 const {connect} = require("./db")
 
-async function  findUser(name){
+async function  findUserByName(name){
         const connection = await connect();
         return connection
                     .collection("users")
                     .findOne({name})//.find{} passa o filtro\; 
-}
+};
+
+async function  findUserByEmail(email){
+    const connection = await connect();
+    return connection
+                .collection("users")
+                .findOne({email})//.find{} passa o filtro\; 
+};
+
+function generatePassword(){
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let password = ""
+
+    for(let i=0; i<10; i++){
+        password += chars.charAt(Math.random()* chars.length)
+    }
+    return password;
+};
 
 module.exports = {
-    findUser
+    findUserByName,
+    findUserByEmail,
+    generatePassword
 }
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../db");
-const { findUser } = require('../auth');
 const bcrypt = require("bcryptjs");
+const auth = require("../auth")
 
 /* GET home page. */
 //usando promisses
@@ -12,9 +12,13 @@ router.get('/', (request, response) => {
   response.render("login",{title:'Login', message:""} );
 })
 
+router.get('/forgot', (request, response) => {
+  response.render("forgot",{title:'Forgot password?', message:""} );
+})
+
 router.post('/login', async (req, res) => { 
   const name = req.body.name;
-  const user = await findUser(name); //função find da auth.js
+  const user = await auth.findUserByName(name); //função find da auth.js
   
   if(!user) return res.render("login", {title:'Login', message:'Usuário e/ou senha INVÁLIDOS'});
 
@@ -23,6 +27,23 @@ router.post('/login', async (req, res) => {
 
   res.redirect("/index");
 
+})
+
+router.post('/forgot', async(req, res) =>{
+  const email = req.body.email;
+  if(!email) 
+    return res.render("forgot", {title:"Forgot password?", message:"Email é OBRIGATÓRIO!"})
+  
+  const user = await auth.findUserByEmail(email); // auth.js
+  if(!user) 
+    return res.render("forgot", {title:"Forgot password?", message:"Email não cadastrado"})
+  
+  const newPassword = auth.generatePassword();
+  user.password = newPassword;
+
+  await db.updateUser(user._id.toString(), user);
+  
+  res.render("forgot", {title:"Forgot password?", message: newPassword}); 
 })
 
 module.exports = router;
